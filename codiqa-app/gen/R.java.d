3e72d9f02305faffff639213377daa61a109d3b7
Ted Behling <ted@tedb.us>
2013-08-24 12:56:05 -0400

    final!  wish us luck!

--- codiqa-app/gen/R.java.d
+++ codiqa-app/gen/R.java.d
@@ -11,13 +11,4 @@
 /Users/tednew/Code/whosin/codiqa-app/res/layout/main.xml \
 /Users/tednew/Code/whosin/codiqa-app/res/values/strings.xml \
 /Users/tednew/Code/whosin/codiqa-app/res/xml/config.xml \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable/icon.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-hdpi/ic_launcher.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-hdpi/icon.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-ldpi/ic_launcher.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-ldpi/icon.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-mdpi/ic_launcher.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-mdpi/icon.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-xhdpi/ic_launcher.png \
-/Users/tednew/Code/whosin/codiqa-app/bin/res/drawable-xhdpi/icon.png \
 /Users/tednew/Code/whosin/codiqa-app/bin/AndroidManifest.xml \
